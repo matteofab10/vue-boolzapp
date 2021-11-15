45fbdaf04d99eed18ea8b036a585087d5280575e
matteofab10 <matteofabio10@gmail.com>
2021-11-15 11:06:49 +0100

    inizio progetto

---
+++ Milestone 1/js/script.js
@@ -0,0 +1,25 @@
+const app = new Vue ({
+
+  el: '#app',
+
+  // data: {
+  //   itemList: [
+  //     {
+  //     text: "Acquistare le scarpe nuove",
+  //     done: false,
+  //   },
+  //   {
+  //     text: "Portare il giubbotto in lavanderia",
+  //     done: false,
+  //   },
+  //   {
+  //     text: "Fare la spesa",
+  //     done: false,
+  //   }
+  //   ],
+  // },
+
+  methods: {
+    
+  }
+})
