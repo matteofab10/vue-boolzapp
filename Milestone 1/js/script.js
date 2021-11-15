0b3b419ce4b350ded23af00ae1ffcfed24ca59be
matteofab10 <matteofabio10@gmail.com>
2021-11-15 18:57:24 +0100

    aggiunta immagine profilo personale

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -2,22 +2,40 @@ const app = new Vue ({
 
   el: '#app',
 
-  // data: {
-  //   itemList: [
-  //     {
-  //     text: "Acquistare le scarpe nuove",
-  //     done: false,
-  //   },
-  //   {
-  //     text: "Portare il giubbotto in lavanderia",
-  //     done: false,
-  //   },
-  //   {
-  //     text: "Fare la spesa",
-  //     done: false,
-  //   }
-  //   ],
-  // },
+  data: {
+    contacts: [
+      {
+        name: 'Erika',
+        avatar: '_1',
+        visible: true,
+        messages: []
+      },
+      {
+        name: 'Stella',
+        avatar: '_2',
+        visible: true,
+        messages: []
+      },
+      {
+        name: 'Christian',
+        avatar: '_3',
+        visible: true,
+        messages: []
+      },
+      {
+        name: 'Barbara',
+        avatar: '_4',
+        visible: true,
+        messages: []
+      },
+      {
+        name: 'Giacomo',
+        avatar: '_5',
+        visible: true,
+        messages: []
+      },
+    ],
+ },
 
   methods: {
     
